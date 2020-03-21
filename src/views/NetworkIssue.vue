14cc36edbf76adb9ced11dc9b74ed867067db716
Naveen Budhwal <naveenbudhwal@gmail.com>
2020-03-21 11:14:05 +0530

    Different views of app

---
+++ src/views/NetworkIssue.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Uh-oh!</h1>
+    <h3>It looks like you are experiencing some network issue. Please click the back button and try again.</h3>
+    <router-link :to="{name: 'event-list'}">Back to home page</router-link>
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
